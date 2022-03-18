af5394478b6026548c93531c5f534a92806927b5
0001010 <icearrows_@naver.com>
2022-03-18 16:48:26 +0900

    Json to String

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from data_crawling import crawling
 from summarizes import summarizes
-import pandas as pd
 
 
 def main(url):
@@ -11,8 +10,4 @@ def main(url):
     for content in contents:
         summary.append(summarizes(content))
 
-    # lists to dataframe & to json
-    df = pd.DataFrame({'content': contents, 'summary': summary})
-    js = df['summary'].to_json(force_ascii = False, indent = 4)
-    
-    return js
+    return ''.join(summary[0])
